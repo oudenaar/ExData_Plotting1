be9a1ae6a1997f3a88fa07482df83fd170defb5e
bvoudenaarde <oudenaar@gmail.com>
2016-01-11 11:33:43 +0100

    small improvement

--- plot2.R
+++ plot2.R
@@ -1,13 +1,16 @@
 # Reading data from file
 pp <- read.table(file="household_power_consumption.txt", sep=";", header=TRUE, colClasses = c("character", "character", "numeric", "numeric","numeric","numeric","numeric","numeric","numeric"), na.strings="?")
 
+# Add DateTime Obj to table
+pp$DateTime<- paste(pp$Date, pp$Time)
+pp$DateTimeObj<-strptime(pp$DateTime,"%d/%m/%Y %H:%M:%S" )
 # Fix First column Data field into real Dates
-pp$Date <- as.Date(pp$Date , "%d/%m/%Y")
+pp$Date<-as.Date(pp$Date , "%d/%m/%Y")
 
 # Only use Dates between 2007-02-01 and 2007-02-02
 pp1 <- subset(pp, Date >= as.Date("2007-02-01") & Date <= as.Date("2007-02-02"))
 
 #Plot to PNG
 png(width = 480, height = 480, units = "px", file = "plot2.png")
-plot(pp1$Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xaxt = "n")
+plot(pp1$DateTimeObj, pp1$Global_active_power, type="l", ylab="Global Active Power (kilowatts)", xlab=" ")
 dev.off()
